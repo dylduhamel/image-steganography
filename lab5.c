116ae95b7b6f5c0341f0447d0f467f2055117e7a
Dylan Duhamel <duhadm19@wfu.edu>
2022-03-23 20:15:02 -0400

    Final commit

--- lab5.c
+++ lab5.c
@@ -11,8 +11,9 @@
  *
  * Compile:
  *   gcc -ansi -pedantic -Wall lab5.c getImageArgs.c ppmReadWrite.c
+ *   ./a.out -b 2 message.txt will.ppm
  *
- * @author Your Name Here {@literal <pluf@wfu.edu>}
+ * @author Your Dylan Duhamel {@literal <pluf@wfu.edu>}
  * @date Feb. 27, 2022
  * @assignment Lab 5
  * @course CSC 250
@@ -32,14 +33,14 @@ void printBits(unsigned char x);
 int main(int argc, char *argv[])
 {
     struct Image *img = NULL;
-    char textFilename[20];
-    char orgImageFilename[20] = {'\0'};
-    char newImageFilename[20] = {'\0'};
+    char textFilename[40];
+    char orgImageFilename[40] = {'\0'};
+    char newImageFilename[40] = {'\0'}; /* what we produce hidden image */
     int bit;
     int argsOk = 0;
     int fileOk = 0;
 
-    argsOk = processHideImageArgs(argc, argv, &bit, textFilename, orgImageFilename); 
+    argsOk = processHideImageArgs(argc, argv, &bit, textFilename, orgImageFilename); /* text file name is message text*/
     if(!argsOk) 
         return 1;
 
@@ -49,29 +50,173 @@ int main(int argc, char *argv[])
 
     hideMessage(img, bit, textFilename);
 
-    /* TODO: need to set newImageFilename */ 
+    /* concatonate _hidden.ppm to the name of the orgImageFilename */
+    strcpy(newImageFilename, orgImageFilename);
+    newImageFilename[strlen(newImageFilename)-4] = '\0';
+    strcat(newImageFilename, "_hidden.ppm");
+
     fileOk = writeImage(newImageFilename, img);
     if(!fileOk) {
         if(img) free(img);  
         return 1;
     }  
+
     /* this print in main is OK, it is AJ certified */
     /* but NO other prints in main are allowed      */
     printf("image saved as %s \n", newImageFilename);  
 
+    if(img -> data) free(img -> data);
     if(img) free(img);  
+
 
     return 0;
 }
 
 
-
+/*  function to hide the message in -b 1, 2, 4
+    takes Image struct, bit specyfier, and the name of file where message is stores
+    returns an integer 1 is no issues
+    edits the LSB's on each RGB value to hide char
+ */
 int hideMessage(struct Image *img, int bit, char textFilename[]) {
+    unsigned char *color = (unsigned char *) img -> data;
+    FILE *filePtr = fopen(textFilename, "rie");
+    unsigned char ch; /* DOES THIS NEED TO BE UNSIGNED?? */
+    int i = 0; /* index */
+    int charIndex = 2; /* begins at 2 because we add :) at the end (2 char) */
 
+    if (filePtr == 0) {
+        printf("file can't be opened \n");
+        return 0;
+    }
+
+    /* getting the initial char */
+    ch = fgetc(filePtr);
+
+    /* 1 bit LSB */
+    if(bit == 1) {
+        /* loop through entire file and process */
+        while(ch < 127 && i < img->width*img->height) {
+            color[i] = (color[i] & 0xFE) | ((ch & 0x80) >> 7);
+            i++;
+            color[i] = (color[i] & 0xFE) | ((ch & 0x40) >> 6);
+            i++;
+            color[i] = (color[i] & 0xFE) | ((ch & 0x2C) >> 5);
+            i++;
+            color[i] = (color[i] & 0xFE) | ((ch & 0x10) >> 4);
+            i++;
+            color[i] = (color[i] & 0xFE) | ((ch & 0x08) >> 3);
+            i++;
+            color[i] = (color[i] & 0xFE) | ((ch & 0x04) >> 2);
+            i++;
+            color[i] = (color[i] & 0xFE) | ((ch & 0x02) >> 1);
+            i++;
+            color[i] = (color[i] & 0xFE) | (ch & 0x01);
+            i++;
+            ch = fgetc(filePtr);
+            charIndex++;
+        }
+        ch = ':';
+        color[i] = (color[i] & 0xFE) | ((ch & 0x80) >> 7);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x40) >> 6);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x2C) >> 5);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x10) >> 4);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x08) >> 3);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x04) >> 2);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x02) >> 1);
+        i++;
+        color[i] = (color[i] & 0xFE) | (ch & 0x01);
+        i++;
+        ch = ')';
+        color[i] = (color[i] & 0xFE) | ((ch & 0x80) >> 7);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x40) >> 6);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x2C) >> 5);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x10) >> 4);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x08) >> 3);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x04) >> 2);
+        i++;
+        color[i] = (color[i] & 0xFE) | ((ch & 0x02) >> 1);
+        i++;
+        color[i] = (color[i] & 0xFE) | (ch & 0x01);
+        i++;
+            
+    /* 2 bit LSB */
+    } else if(bit == 2) {
+        /* loop through entire file and process */
+        while(ch < 127 && i < img->width*img->height) {
+            color[i] = (color[i] & 0xFC) | ((ch & 0xC0) >> 6);
+            i++;
+            color[i] = (color[i] & 0xFC) | ((ch & 0x30) >> 4);
+            i++;
+            color[i] = (color[i] & 0xFC) | ((ch & 0x0C) >> 2);
+            i++;
+            color[i] = (color[i] & 0xFC) | (ch & 0x03);
+            i++;
+            ch = fgetc(filePtr);
+            charIndex++;
+        }
+        /* adding the :) to the end */
+        ch = ':';
+        color[i] = (color[i] & 0xFC) | ((ch & 0xC0) >> 6);
+        i++;
+        color[i] = (color[i] & 0xFC) | ((ch & 0x30) >> 4);
+        i++;
+        color[i] = (color[i] & 0xFC) | ((ch & 0x0C) >> 2);
+        i++;
+        color[i] = (color[i] & 0xFC) | (ch & 0x03);
+        i++;
+        ch = ')';
+        color[i] = (color[i] & 0xFC) | ((ch & 0xC0) >> 6);
+        i++;
+        color[i] = (color[i] & 0xFC) | ((ch & 0x30) >> 4);
+        i++;
+        color[i] = (color[i] & 0xFC) | ((ch & 0x0C) >> 2);
+        i++;
+        color[i] = (color[i] & 0xFC) | (ch & 0x03);
+        i++;
+    
+    /* 4 bit LSB */
+    } else if(bit == 4) {
+        /* loop through entire file and process */
+        while(ch < 127 && i < img->width*img->height) {
+            color[i] = (color[i] & 0xf0) | ((ch & 0xf0) >> 4);
+            i++;
+            color[i] = (color[i] & 0xf0) | (ch & 0x0f);
+            i++;
+            ch = fgetc(filePtr);
+            charIndex++;
+        }
+        /* adding the :) to the end */
+        ch = ':';
+        color[i] = (color[i] & 0xf0) | ((ch & 0xf0) >> 4);
+        i++;
+        color[i] = (color[i] & 0xf0) | (ch & 0x0f);
+        i++;
+        ch = ')';
+        color[i] = (color[i] & 0xf0) | ((ch & 0xf0) >> 4);
+        i++;
+        color[i] = (color[i] & 0xf0) | (ch & 0x0f);
+        i++;
+    }
+
+    printf("hid %d characters in %d color components (%d pixels)\n", charIndex, i, (i + 2)/3);
+
+    fclose(filePtr);
     return 1;
 }
 
-
+/* prints bit info for char input */
 void printBits(unsigned char x) {
     int value = x;
     char *bits;
